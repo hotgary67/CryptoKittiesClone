3698642b795b6898b3c144b06cc247f5ce12f36c
hotgary67 <69466703+hotgary67@users.noreply.github.com>
2020-09-17 15:31:56 +0200

    Initial commit

---
+++ addresses.js
@@ -0,0 +1,4 @@
+binance_keys = {
+	'api_key': "eXTkNogyb3pacBtTgAD8XvghJCamde8BLT0ZvUO4zxrec6h5VvqILPsr4GwrROiF",
+	'secret_key': "keDDgh7QMhRrk1O8mBwWzOP2kT2JemkQdMTn0p7oehGFauDoXftIJXhGvgUqOZP4"
+}
